ac2ffec106a073ce78553bac9c30517668bec80c
mengzhim <91888942+mengzhim@users.noreply.github.com>
2024-01-17 11:45:33 +0800

    Add files via upload

---
+++ FirstSample.java
@@ -0,0 +1,10 @@
+package p3;
+
+public class FirstSample {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+//Java中区分大小写，包中的类名需要与代码中的类名称相符合，类名应采取驼峰命名法
+//println()和print()的区别是，后者打印字符串需要不会换行
+//执行某个类时，总是从main()方法开始执行
